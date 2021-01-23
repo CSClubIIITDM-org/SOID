efa067491bcacd12e5769baf22cbb6b6d336f22d
salman bin salahudeen <salmanbinsalahudeen@gmail.com>
2021-01-23 23:10:25 +0530

    navbar

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+import Menubar from "./components/Menubar";
+
+function App() {
+  return (
+
+
+
+    <Menubar/>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //       <p>new App</p>
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
