f205d03bdd881f02ea549dcf211388b89a81adb9
Aniket Rochwani <aniket.rochwani2000@gmail.com>
2021-01-24 04:09:11 +0530

    update contact page

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import Menubar from "../components/Menubar";
+import ContactInfo from "../components/ContactInfo";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Contact = () => {
-  return <Menubar />;
+  return (
+    <React.Fragment>
+      <Menubar />
+      <Container>
+        <Row style={{ marginTop: "80px" }}>
+          <Col>
+            <ContactInfo />
+          </Col>
+          <Col style={{ marginTop: "100px" }}>
+            <ContactInfo />
+          </Col>
+          <Col>
+            <ContactInfo />
+          </Col>
+        </Row>
+        <h1 className="contact-heading">Design Advisory Council</h1>
+        <Row>
+          <Col>
+            <ContactInfo />
+          </Col>
+          <Col style={{ marginTop: "100px" }}>
+            <ContactInfo />
+          </Col>
+          <Col>
+            <ContactInfo />
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Contact;
