05964d8c9ceb795f8633491e2f60c074cb2c6a3d
Aniket Rochwani <aniket.rochwani2000@gmail.com>
2021-01-28 16:40:34 +0530

    update carousel img src

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const About = () => {
   return (
     <React.Fragment>
-      <div className="bg-secondary about-section">
+      <div className="bg-secondary about-section mb-0">
         <h1 className="about-heading">What is SIDI ?</h1>
-        <p className="about-description">
+        <p className="about-description mb-0">
           <span>
             The School of Interdisciplinary Design and Innovation (SIDI)
           </span>{" "}
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/custom-animations/cube-animation.css";
+import Picture from "../images/EDII-TN_Visit_MaDeIT.jpg";
 
 const Slider = () => {
   return (
@@ -12,25 +13,13 @@ const Slider = () => {
       >
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img
-              className="d-block w-100"
-              src="http://www.madeit.iiitdm.ac.in/img/EDII-TN_Visit_MaDeIT.jpg"
-              alt="First slide"
-            />
+            <img className="d-block w-100" src={Picture} alt="First slide" />
           </div>
           <div className="carousel-item">
-            <img
-              className="d-block w-100"
-              src="http://www.madeit.iiitdm.ac.in/img/EDII-TN_Visit_MaDeIT.jpg"
-              alt="Second slide"
-            />
+            <img className="d-block w-100" src={Picture} alt="Second slide" />
           </div>
           <div className="carousel-item">
-            <img
-              className="d-block w-100"
-              src="http://www.madeit.iiitdm.ac.in/img/EDII-TN_Visit_MaDeIT.jpg"
-              alt="Third slide"
-            />
+            <img className="d-block w-100" src={Picture} alt="Third slide" />
           </div>
         </div>
         <h1 className="carousel-heading">
