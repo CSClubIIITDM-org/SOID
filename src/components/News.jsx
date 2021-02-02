897461b6bb22cd70c1c17883c8b69c073a70b72e
shelcia <shelcia1999@gmail.com>
2021-02-03 00:29:33 +0530

    update

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,138 +1,126 @@
 import React from "react";
 
-import Carousel from 'react-elastic-carousel';
+import Carousel from "react-elastic-carousel";
 
 import Container from "react-bootstrap/Container";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
 const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 3 }
-  ];
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 3 },
+];
 
-
-
-function News() {
-    
-
-    return(
-        <div className="news-container">
-        <Container>
+const News = () => {
+  return (
+    <div className="news-container">
+      <Container>
         <h4 className="news text-light text-center pb-5 pt-5">News & Events</h4>
-        <Carousel breakPoints={breakPoints} itemPadding={[10, 10]}
-        
-         >
-        <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-      <div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-  <Card className= "bg-orange text-light text-center p-3" >
-      <p className="mt-2 ml-2 mb-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>  
-  </Card>
-      </Carousel>
+        <Carousel breakPoints={breakPoints} itemPadding={[10, 10]}>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+            <div className="modal" tabIndex="-1">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Modal title</h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <p>Modal body text goes here.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+          <Card className="bg-orange text-light text-center p-3">
+            <p className="mt-2 ml-2 mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Integer posuere erat a
+              ante.
+            </p>
+          </Card>
+        </Carousel>
       </Container>
-      </div>
-
-    );
-}
+    </div>
+  );
+};
 
 export default News;
--- src/components/ReactLoader.jsx
+++ src/components/ReactLoader.jsx
@@ -4,7 +4,7 @@ import PropagateLoader from "react-spinners/PropagateLoader";
 const ReactLoader = ({ loading }) => {
   return (
     <React.Fragment>
-      <div>
+      <div className="d-flex justify-content-between align-items-center">
         <PropagateLoader color="#ff652f" loading={loading} size={50} />
       </div>
     </React.Fragment>
